3c989ad29e5378109e5a1713990dbc600753085d
mikomgk <mikomgk@gmail.com>
2018-07-21 18:54:21 +0300

    project

---
+++ Assembler/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "settings.h"
+
+#define err_1 "ergfror fld fds"
+
+void *mapping(char *word, char *words[], void *params[]);
+
+int trim(char *word, int in_brackets);
+
+void dec2bin(int num,char *word);
+
+int main() {
+    char *a="    dsa         fdsfds         ,        daf     ,  dsa         ";
+    trim(a,0);
+    puts(a);
+    return 0;
+}
+
+void *mapping(char *word, char *words[], void *params[]) {
+    if (!word)
+        return NULL;
+    for (; *words; words++, params++)
+        if (!strcmp(word, *words))
+            return *params;
+    return NULL;
+}
+
+int trim(char *word, int in_brackets) {
+    char *ptr = word, *start = word, *tmp;
+    int trimmed_flag = 0;
+    for (; isspace(*word); word++)
+        ;
+    //TODO  check here
+    for (; *word; *ptr++ = *word++)
+        ;
+    //TODO  check here
+    for (--word; isspace(*ptr); ptr--)
+        ;
+    *++ptr = '\0';
+    for (ptr = word = start; *word; *ptr++ = *word++) {
+        if (!in_brackets && *word == '\"') {
+            for (; *word != '\"'; *ptr++ = *word++)
+                ;
+            continue;
+        }
+        if (!in_brackets && *word == '(') {
+            for (; *word != ')'; *ptr++ = *word++)
+                ;
+            continue;
+        }
+        if (isspace(*word)) {
+            tmp = word - 1;
+            for (; isspace(*word); word++)
+                ;
+            if (*word != ',' && *tmp != ',')
+                *--word = ' ';
+            else
+                *--word = *--ptr;
+            trimmed_flag = 1;
+        }
+    }
+    for (; *word; *ptr++ = *word++)
+        ;
+    *ptr = '\0';
+    return trimmed_flag;
+}
+
+void dec2bin(int num,char *word) {
+    int mask = 1, i;
+    for (i = NUMBER_SIZE - 1; i >= 0; i--, mask <<= 1)
+        if (num & mask)
+            word[i] = '/';
+        else
+            word[i] = '.';
+}
---
+++ Assembler/settings.h
@@ -0,0 +1,27 @@
+#ifndef ASSEMBLER_SETTINGS_H
+#define ASSEMBLER_SETTINGS_H
+
+#define WORD_SIZE 14
+#define NUMBER_SIZE 12
+
+char *addressing_13[] = {"1", "3"};
+char *addressing_013[] = {"0", "1", "3"};
+char *addressing_123[] = {"1", "2", "3"};
+char *addressing_1[] = {"1"};
+
+char *op_names[] = {"mov", "cmp", "add", "sub", "not", "clr", "lea", "inc", "dec", "jmp", "bne", "red", "prn", "jsr", "rts", "stop", NULL};
+char *op_code[] = {"....", ".../", "../.", "..//", "./..", "././", ".//.", ".///", "/...", "/../", "/./.", "/.//", "//..", "//./", "///.", "////", NULL};
+char *num_of_operands_per_op[] = {"2", "2", "2", "2", "1", "1", "2", "1", "1", "1", "1", "1", "1", "1", "0", "0", NULL};
+char **dest_operand_addressing_types_per_op[] = {addressing_13, addressing_013, addressing_13, addressing_13, addressing_13, addressing_13, addressing_13,
+                                                 addressing_13, addressing_13, addressing_123, addressing_123, addressing_13, addressing_013,
+                                                 addressing_123, NULL, NULL, NULL};
+char **src_operand_addressing_types_per_op[] = {addressing_013, addressing_013, addressing_013, addressing_013, NULL, NULL, addressing_1, NULL, NULL, NULL,
+                                                NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+char *register_names[] = {"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", NULL};
+char *register_code[] = {"......", "...../", "..../.", "....//", ".../..", "..././", "...//.", "...///", NULL};
+char *addressing_types[] = {"0", "1", "2", "3", NULL};
+char *addressing_types_code[] = {"..", "./", "/.", "//", NULL};
+char *are[] = {"A", "R", "E"};
+char *are_code[] = {"..", "/.", "./"};
+
+#endif //ASSEMBLER_SETTINGS_H
